2ab22ac1e5bfea940f0545693fb9c8bb8b2a12a7
Big Slappy <126707707+JoseDavid12345@users.noreply.github.com>
2023-11-14 10:03:54 -0600

    yipi

---
+++ si_resources/js/ardoraAforCFG.js
@@ -0,0 +1,32 @@
+//Creado con Ardora - www.webardora.net
+//bajo licencia Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0)
+//para otros usos contacte con el autor
+var timeAct=360; timeIni=360; timeBon=0;
+var successes=0; successesMax=7; attempts=0; attemptsMax=1;
+var score=0; scoreMax=7; scoreInc=1; scoreDec=0;
+var typeGame=0;
+var tiTime=true;
+var tiTimeType=1;
+var tiButtonTime=true;
+var textButtonTime="Comenzar";
+var tiSuccesses=false;
+var tiAttempts=false;
+var tiScore=false;
+var startTime;
+var colorBack="#FFFDFD"; colorButton="#91962F"; colorText="#000000"; colorSele="#FF8000";
+var goURLNext=false; goURLRepeat=false;tiAval=false;
+var scoOk=0; scoWrong=0; scoOkDo=0; scoWrongDo=0; scoMessage=""; scoPtos=10;
+var fMenssage="Verdana, Geneva, sans-serif";
+var fActi="Verdana, Geneva, sans-serif";
+var fEnun="Verdana, Geneva, sans-serif";
+var timeOnMessage=5; messageOk="YIPI"; messageTime="INTENTA OTRA VEZ"; messageError="ERROR"; messageErrorG="ERROR"; messageAttempts=""; isShowMessage=false;
+var urlOk=""; urlTime=""; urlError=""; urlAttempts="";
+var goURLOk="_blank"; goURLTime="_blank"; goURLAttempts="_blank"; goURLError="_blank"; 
+borderOk="#008000"; borderTime="#FF0000";borderError="#FF0000"; borderAttempts="#FF0000";
+var wordsGame="c2k="; wordsStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+function giveZindex(typeElement){var valueZindex=0; capas=document.getElementsByTagName(typeElement);
+for (i=0;i<capas.length;i++){if (parseInt($(capas[i]).css("z-index"),10)>valueZindex){valueZindex=parseInt($(capas[i]).css("z-index"),10);}}return valueZindex;}
+var words=["QU5HVUxP","Q1VBRFJBRE8=","Q09TRU5P","U0VOTw==","U1VNQQ==","UkVTVEE=","SElQT1RFTlVTQQ==","","",""];imaW=["","","","","","","","","",""];queW=["esta entre 2 lados","tiene 4 lados","Es una ley de los angulos y lados","Es otra ley de los angulos y lados","Cuando una cantidad x se agrega a otra cantidad x","cuando una casntidad x se le quita a otra cantidad x","Es el lado que esta en frente de el angulo recto","","",""];var wordsL=[6,8,6,4,4,5,10,"","",""];var altW=["","","","","","","","","",""];
+var auW=["","","","","","","","","",""];
+var fillLetter="ABCDEFGHIJKLMNÑOPQRSTUVWXYZÁÉÍÓÚÜ "; wordsG=[];var wordsGL=[]; indexG=0; actMaxWidth=550; indexWord=0; totalWord=0;
+var alterW=[];var imageW=[];questionW=[];audioW=[];profG=0; dirMedia="si_resources/media/"; textBNext="";
